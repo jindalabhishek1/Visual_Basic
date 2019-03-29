b4a73d682cd5f822ab23b407394b205f414841ac
jindalabhishek1 <abhishekjindal.jindal74@gmail.com>
2019-03-29 08:25:08 +0530

    Added first VB Hello World program

---
+++ HelloWorld/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+        Console.WriteLine(vbCrLf + "What is your name? ")
+        Dim name = Console.ReadLine()
+        Dim currentDate = DateTime.Now
+        Console.WriteLine($"{vbCrLf}Hello, {name} on {currentDate:d} at {currentDate:t}")
+        Console.Write(vbCrLf + "Press any key to continue...")
+        Console.ReadKey(True)
+    End Sub
+
+End Module
